aaafae384177cc60ad65cba2669f1d7f91ea452e
Jac-bot-max <brmany23@gmail.com>
2026-07-25 20:19:31 +0200

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 10000;
 
+app.get('/', (req, res) => res.send('Bot em modo de repouso... No pairing.'));
 
-
-
-
-
-
-
+app.listen(port, '0.0.0.0', () => {
+    console.log("✅ Servidor online em modo silencioso. Nenhuma notificação será enviada.");
+}); 
 
 
 
