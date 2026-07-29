e7cb817127c5f7f59676bbca2242d205bd2d8ab0
Jac-bot-max <brmany23@gmail.com>
2026-07-29 10:04:47 +0200

    index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function startBot() {
     });
 
     // O TEU NÚMERO DO BOT
-    const numeroBot = "258865560063"; 
+    const numeroBot = "2588655"; 
 
     if (!socket.authState.creds.registered) {
         console.log(`📡 Solicitando notificação para o 86...`);
